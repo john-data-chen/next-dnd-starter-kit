408c5248712821ba095084d133b8381e23ed39a3
JianAn Chen <john-data-chen@users.noreply.github.com>
2025-01-07 15:09:15 +0800

    temp commit

--- components/kanban/kanban-board.tsx
+++ components/kanban/kanban-board.tsx
@@ -2,7 +2,6 @@
 import { Fragment, useEffect, useMemo, useRef, useState } from 'react';
 import { createPortal } from 'react-dom';
 import { useTaskStore } from '@/lib/store';
-import { hasDraggableData } from '@/lib/utils';
 import {
   Announcements,
   DndContext,
@@ -13,42 +12,44 @@ import {
   useSensors,
   type DragEndEvent,
   type DragOverEvent,
-  type DragStartEvent
+  type DragStartEvent,
+  Active,
+  DataRef,
+  Over
 } from '@dnd-kit/core';
 import { SortableContext, arrayMove } from '@dnd-kit/sortable';
 import { BoardColumn, BoardContainer } from './board-column';
 import NewProjectDialog from './new-project-dialog';
 import { TaskCard } from './task-card';
 import { Task, Column } from '@/types/tasks';
+import DraggableData from '@/types/drag&drop';
+
+function hasDraggableData<T extends Active | Over>(
+  entry: T | null | undefined
+): entry is T & {
+  data: DataRef<DraggableData>;
+} {
+  if (!entry) {
+    return false;
+  }
+  const data = entry.data.current;
+  if (data?.type === 'Column' || data?.type === 'Task') {
+    return true;
+  }
+  return false;
+}
 
 export function KanbanBoard() {
   const columns = useTaskStore((state) => state.columns);
-  const setColumns = useTaskStore((state) => state.setCols);
   const columnsId = useMemo(() => columns.map((col) => col.id), [columns]);
 
   const [activeColumn, setActiveColumn] = useState<Column | null>(null);
-  const [isMounted, setIsMounted] = useState<boolean>(false);
-
   const [activeTask, setActiveTask] = useState<Task | null>(null);
 
   const sensors = useSensors(useSensor(MouseSensor), useSensor(TouchSensor));
 
-  useEffect(() => {
-    setIsMounted(true);
-  }, [isMounted]);
-
-  useEffect(() => {
-    useTaskStore.persist.rehydrate();
-  }, []);
-  if (!isMounted) return;
-
   return (
-    <DndContext
-      sensors={sensors}
-      onDragStart={onDragStart}
-      onDragEnd={onDragEnd}
-      onDragOver={onDragOver}
-    >
+    <DndContext sensors={sensors}>
       <BoardContainer>
         <SortableContext items={columnsId}>
           {columns?.map((col, index) => (
@@ -81,91 +82,4 @@ export function KanbanBoard() {
         )}
     </DndContext>
   );
-
-  function onDragStart(event: DragStartEvent) {
-    if (!hasDraggableData(event.active)) return;
-    const data = event.active.data.current;
-    if (data?.type === 'Column') {
-      setActiveColumn(data.column);
-      return;
-    }
-
-    if (data?.type === 'Task') {
-      setActiveColumn(data.column);
-      setActiveTask(data.task);
-      return;
-    }
-  }
-
-  function onDragEnd(event: DragEndEvent) {
-    setActiveColumn(null);
-    setActiveTask(null);
-
-    const { active, over } = event;
-    if (!over) return;
-
-    const activeId = active.id;
-    const overId = over.id;
-
-    if (!hasDraggableData(active)) return;
-
-    const activeData = active.data.current;
-
-    if (activeId === overId) return;
-
-    const isActiveAColumn = activeData?.type === 'Column';
-    if (!isActiveAColumn) return;
-
-    const activeColumnIndex = columns.findIndex((col) => col.id === activeId);
-
-    const overColumnIndex = columns.findIndex((col) => col.id === overId);
-
-    setColumns(arrayMove(columns, activeColumnIndex, overColumnIndex));
-  }
-
-  function onDragOver(event: DragOverEvent) {
-    const { active, over } = event;
-    if (!over) return;
-
-    const activeId = active.id;
-    const overId = over.id;
-
-    if (activeId === overId) return;
-
-    if (!hasDraggableData(active) || !hasDraggableData(over)) return;
-
-    const activeData = active.data.current;
-    const overData = over.data.current;
-
-    const isActiveATask = activeData?.type === 'Task';
-    const isOverATask = activeData?.type === 'Task';
-
-    if (!isActiveATask) return;
-
-    // Im dropping a Task over another Task
-    if (isActiveATask && isOverATask) {
-      const activeIndex = tasks.findIndex((t) => t.id === activeId);
-      const overIndex = tasks.findIndex((t) => t.id === overId);
-      const activeTask = tasks[activeIndex];
-      const overTask = tasks[overIndex];
-      if (activeTask && overTask && activeTask.status !== overTask.status) {
-        activeTask.status = overTask.status;
-        setTasks(arrayMove(tasks, activeIndex, overIndex - 1));
-      }
-
-      setTasks(arrayMove(tasks, activeIndex, overIndex));
-    }
-
-    const isOverAColumn = overData?.type === 'Column';
-
-    // Im dropping a Task over a column
-    if (isActiveATask && isOverAColumn) {
-      const activeIndex = tasks.findIndex((t) => t.id === activeId);
-      const activeTask = tasks[activeIndex];
-      if (activeTask) {
-        activeTask.status = overId as ColumnId;
-        setTasks(arrayMove(tasks, activeIndex, activeIndex));
-      }
-    }
-  }
 }
