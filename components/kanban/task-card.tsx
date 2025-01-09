f20b4491f7f74b4c7adb3fa7e6b138ba2e1438e6
JianAn Chen <john-data-chen@users.noreply.github.com>
2025-01-09 15:48:18 +0800

    Remove drag button, make task component can be dragged

--- components/kanban/task-card.tsx
+++ components/kanban/task-card.tsx
@@ -1,10 +1,8 @@
-import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardHeader } from '@/components/ui/card';
 import { Task } from '@/types/tasks';
 import { useSortable } from '@dnd-kit/sortable';
 import { CSS } from '@dnd-kit/utilities';
 import { cva } from 'class-variance-authority';
-import { GripVertical } from 'lucide-react';
 import { Badge } from '@/components/ui/badge';
 
 interface TaskCardProps {
@@ -56,20 +54,13 @@ export function TaskCard({ task, isOverlay }: TaskCardProps) {
     <Card
       ref={setNodeRef}
       style={style}
+      {...attributes}
+      {...listeners}
       className={variants({
         dragging: isOverlay ? 'overlay' : isDragging ? 'over' : undefined
       })}
     >
       <CardHeader className="space-between relative flex flex-row border-b-2 border-secondary px-3 py-3">
-        <Button
-          variant={'ghost'}
-          {...attributes}
-          {...listeners}
-          className="-ml-2 h-auto cursor-grab p-1 text-secondary-foreground/50"
-        >
-          <span className="sr-only">Move task</span>
-          <GripVertical />
-        </Button>
         <Badge variant={'outline'} className="ml-auto font-semibold">
           Task
         </Badge>
