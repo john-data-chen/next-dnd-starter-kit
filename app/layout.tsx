5f08fc3d5c02e5b34257cdd8fe801bf08e14df3b
JianAn Chen <john-data-chen@users.noreply.github.com>
2025-01-13 14:30:38 +0800

    remove meta info

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,9 @@
 import { auth } from '@/lib/auth';
 import Providers from '@/components/layout/providers';
 import { Toaster } from '@/components/ui/sonner';
-import type { Metadata } from 'next';
 import { Lato } from 'next/font/google';
 import NextTopLoader from 'nextjs-toploader';
 import './globals.css';
-
-export const metadata: Metadata = {
-  title: 'Next Shadcn',
-  description: 'Basic dashboard with Next.js and Shadcn'
-};
 
 const lato = Lato({
   subsets: ['latin'],
