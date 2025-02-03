e9b91a05150b3b814ada41c133e3a6f6f68f6fea
JianAn Chen <john.data.chen@gmail.com>
2025-02-03 09:57:20 +0800

    feat: add date pciker in new task dialog

---
+++ src/components/CalendarForm.tsx
@@ -0,0 +1,87 @@
+'use client';
+
+import { zodResolver } from '@hookform/resolvers/zod';
+import { format } from 'date-fns';
+import { CalendarIcon } from 'lucide-react';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+
+import { cn } from '@/lib/utils';
+import { toast } from 'sonner';
+import { Button } from '@/components/ui/button';
+import { Calendar } from '@/components/ui/calendar';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage
+} from '@/components/ui/form';
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger
+} from '@/components/ui/popover';
+
+const FormSchema = z.object({
+  dueDate: z.date({
+    required_error: 'A due date is required.'
+  })
+});
+
+export default function CalendarForm() {
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema)
+  });
+
+  function onSubmit(data: z.infer<typeof FormSchema>) {
+    toast(data.dueDate.toString());
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="dueDate"
+          render={({ field }) => (
+            <FormItem className="flex flex-col">
+              <FormLabel>Due Date</FormLabel>
+              <Popover>
+                <PopoverTrigger asChild>
+                  <FormControl>
+                    <Button
+                      variant={'outline'}
+                      className={cn(
+                        'w-[240px] pl-3 text-left font-normal',
+                        !field.value && 'text-muted-foreground'
+                      )}
+                    >
+                      {field.value ? (
+                        format(field.value, 'PPP')
+                      ) : (
+                        <span>Pick a date</span>
+                      )}
+                      <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                    </Button>
+                  </FormControl>
+                </PopoverTrigger>
+                <PopoverContent className="w-auto p-0" align="start">
+                  <Calendar
+                    mode="single"
+                    selected={field.value}
+                    onSelect={field.onChange}
+                    fromDate={new Date()}
+                    initialFocus
+                  />
+                </PopoverContent>
+              </Popover>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+      </form>
+    </Form>
+  );
+}
--- src/components/kanban/NewTaskDialog.tsx
+++ src/components/kanban/NewTaskDialog.tsx
@@ -14,6 +14,7 @@ import { Input } from '@/components/ui/input';
 import { Textarea } from '@/components/ui/textarea';
 import { useTaskStore } from '@/lib/store';
 import React from 'react';
+import CalendarForm from '@/components/CalendarForm';
 
 export interface NewTaskDialogProps {
   projectId: string;
@@ -92,6 +93,9 @@ export default function NewTaskDialog({
               onChange={handleTitleChange}
               data-testid="task-title-input"
             />
+          </div>
+          <div className="grid grid-cols-4 items-center gap-4">
+            <CalendarForm />
           </div>
           <div className="grid grid-cols-4 items-center gap-4">
             <Textarea
