65ef22e2df20908a662d496b2ab0347867e9e227
JianAn Chen <john.data.chen@gmail.com>
2025-03-18 15:12:01 +0800

    chore: change middleware path

--- src/app/middleware.ts
+++ src/app/middleware.ts
@@ -1,19 +1,16 @@
 import { ROUTES } from '@/constants/routes';
+import authConfig from '@/lib/auth/auth.config';
 import NextAuth from 'next-auth';
-import authConfig from './auth/auth.config';
 
 const { auth } = NextAuth(authConfig);
 
 export default auth((req) => {
   if (!req.auth) {
-    const url = req.url.replace(req.nextUrl.pathname, ROUTES.HOME);
+    const url = req.url.replace(req.nextUrl.pathname, ROUTES.AUTH.LOGIN);
     return Response.redirect(url);
   }
 });
 
 export const config = {
-  matcher: [
-    '/kanban/:path*', // Protect kanban routes
-    '/((?!login).*)' // Protect all routes except auth
-  ]
+  matcher: ['/boards/:path*', '/((?!login).*)']
 };
