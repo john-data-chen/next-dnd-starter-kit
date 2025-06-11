cc6b9d999e3f49b95439c029a8d6ac5d462ed392
JianAn Chen <john.data.chen@gmail.com>
2025-06-11 16:56:33 +0800

    fix: update the layout of small screens to fix issue of Breadcrumbs doesn't have spaces

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -7,14 +7,17 @@ import { UserNav } from './UserNav';
 
 export default function Header() {
   return (
-    <header className="flex h-16 shrink-0 items-center justify-between gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
-      <div className="flex items-center gap-2 px-4">
+    <header className="flex h-auto shrink-0 flex-wrap items-center justify-between gap-x-4 gap-y-2 p-2 md:h-16 md:flex-nowrap md:px-4 transition-all ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
+      <div className="flex items-center gap-2">
         <SidebarTrigger className="-ml-1" />
         <Separator orientation="vertical" className="mr-2 h-4" />
+      </div>
+
+      <div className="order-last w-full flex-grow md:order-none md:w-auto">
         <Breadcrumbs />
       </div>
 
-      <div className="flex items-center gap-2 px-4">
+      <div className="flex items-center gap-2">
         <UserNav />
         <ThemeToggle />
         <LanguageSwitcher />
