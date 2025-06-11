3bf4f2edad58c2b085a9a8d9ec4082ded78e729f
JianAn Chen <john.data.chen@gmail.com>
2025-06-11 14:50:59 +0800

    feat: update kanban overview localization

--- src/constants/pageMetaData.ts
+++
@@ -1,9 +0,0 @@
-export const projectMetaData = {
-  title: 'Next Project Manager',
-  description: 'Next.js template for beginner'
-};
-
-export const kanbanOverview = {
-  title: 'Kanban Overview',
-  description: 'Manage your boards in Overview'
-};
